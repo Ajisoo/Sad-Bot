e98928f87c98789f15e04e91f82073619df984a7
Joshua Cheng <joshua.bj.cheng@gmail.com>
2020-10-01 13:27:18 -0700

    Add splash guessing

--- data/bot_status.py
+++ data/bot_status.py
@@ -2,10 +2,10 @@ class BotStatus:
 
 	def __init__(self, discord_client):
 		self.client = discord_client
-		self.ga_valid = True
-		self.gs_valid = True
-		self.ga_answer = ""
-		self.ga_answer_raw = ""
+		self.g_valid = True
+		self.g_answer = ""
+		self.g_answer_raw = ""
 		self.patch_message_sent = False
 		self.current_Franklins = {}
 		self.listener = None
+		self.guess_type = ""

dcb26c29493b37890756089776c322f0f5fc965b
Joshua Cheng <joshua.bj.cheng@gmail.com>
2021-04-21 11:24:20 -0500

    fix directory creation bug

--- commands/roll_color.py
+++ commands/roll_color.py
@@ -11,6 +11,7 @@ from PIL import Image
 
 def create_user_data_file():
 	if not os.path.exists("content/roll_color/user_data.txt"):
+		os.makedirs("content/roll_color/")
 		with open("content/roll_color/user_data.txt", "w+") as f:
 			pass
 

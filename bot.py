a7d8a497ee585d7751b9381674637975b81ad702
Andrew Chang <Ajisoo.chang@gmail.com>
2022-02-28 11:56:30 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,6 +72,7 @@ async def on_message(message: discord.Message):
 	if message.author.status == discord.Status.dnd:
 		await message.add_reaction(await message.guild.fetch_emoji(634171978420322335))
 		await message.add_reaction(await message.guild.fetch_emoji(851961972840726578))
+		await message.add_reaction('🍰')
 
 	if message.author.id in BIRTHDAYS.keys() and BIRTHDAYS[message.author.id][0] == now.month and BIRTHDAYS[message.author.id][1] == now.day:
 		await message.add_reaction('🍰')

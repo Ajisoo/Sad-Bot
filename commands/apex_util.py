eb6f171662a1cfc9b419455830381ac9a7b47ae1
Andrew Chang <Ajisoo.chang@gmail.com>
2022-09-05 11:32:01 -0700

    Added caching 1 min

--- commands/apex_util.py
+++ commands/apex_util.py
@@ -1,24 +1,37 @@
 # import discord
 import requests
+import time
 
 from bs4 import BeautifulSoup
 
 
 from globals import APEX_MAP_ROTATION_SITE
 
+next_time = None
+cached_value = None
+
 
 async def cmd_get_apex_map(message):
-	r = requests.get(APEX_MAP_ROTATION_SITE)
+	global next_time
+	global cached_value
+	print("current time" + str(time.time()))
+	if next_time is None or cached_value is None or next_time < time.time():
+		print("no cache")
+		r = requests.get(APEX_MAP_ROTATION_SITE)
 
-	if int(r.status_code / 100) != 2:
-		return
+		if int(r.status_code / 100) != 2:
+			await message.channel.send('Error: ' + r.status_code)
+			return
 
-	soup_value = BeautifulSoup(r.content, "html.parser")
 
-	map_title = soup_value.find("div", {"class": "col-lg-6"}) \
-					.find("div", {"onclick": "location.href = '/current-map/battle_royale/pubs';"}) \
-					.find("h1")
+		soup_value = BeautifulSoup(r.content, "html.parser")
 
-	await message.channel.send('Current map (' + map_title.text + ')')
+		cached_value = soup_value.find("div", {"class": "col-lg-6"}) \
+						.find("div", {"onclick": "location.href = '/current-map/battle_royale/pubs';"}) \
+						.find("h1")
+		next_time = time.time() + 60 # 1 min
+		print("new time" + str(next_time))
+
+	await message.channel.send('Current map (' + cached_value.text + ')')
 
 

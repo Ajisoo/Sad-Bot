59e0416a3d9889e375871c179c4d0fbf2b5f75e3
Jonathan Shi <jhshi07@gmail.com>
2022-09-06 00:16:48 -0700

    Add timer for apex maps

--- commands/apex_util.py
+++ commands/apex_util.py
@@ -1,37 +1,43 @@
 # import discord
-import requests
+import datetime as dt
 import time
 
+import requests
 from bs4 import BeautifulSoup
 
+from globals import get_key, API_ENDPOINTS
 
-from globals import APEX_MAP_ROTATION_SITE
-
-next_time = None
-cached_value = None
+curr_map_name = None
+curr_map_end_ms = None
+next_map_name = None
+next_map_end_ms = None
 
 
 async def cmd_get_apex_map(message):
-	global next_time
-	global cached_value
-	print("current time" + str(time.time()))
-	if next_time is None or cached_value is None or next_time < time.time():
-		print("no cache")
-		r = requests.get(APEX_MAP_ROTATION_SITE)
-
-		if int(r.status_code / 100) != 2:
-			await message.channel.send('Error: ' + r.status_code)
-			return
-
-
-		soup_value = BeautifulSoup(r.content, "html.parser")
-
-		cached_value = soup_value.find("div", {"class": "col-lg-6"}) \
-						.find("div", {"onclick": "location.href = '/current-map/battle_royale/pubs';"}) \
-						.find("h1")
-		next_time = time.time() + 60 # 1 min
-		print("new time" + str(next_time))
-
-	await message.channel.send('Current map (' + cached_value.text + ')')
-
-
+	# should probably be made atomic somehow but who cares
+	global curr_map_name
+	global curr_map_end_ms
+	global next_map_name
+	global next_map_end_ms
+	now = int(time.time())
+	# Check cached map data
+	if curr_map_end_ms is not None and now > curr_map_end_ms and next_map_end_ms is not None:
+		# Use cached next map, do request later
+		curr_map_name = next_map_name
+		curr_map_end_ms = next_map_end_ms
+		next_map_name = None
+		next_map_end_ms = None
+		return
+	key = get_key("apex")
+	endpoint_url = API_ENDPOINTS["apex_map"]
+	r = requests.get(endpoint_url, headers={"Authorization": key})
+	if r.status_code == 200:
+		json = r.json()
+		curr_map_name = json["current"]["map"]
+		curr_map_end_ms = json["current"]["end"]
+		next_map_name = json["next"]["map"]
+		next_map_end_ms = json["next"]["end"]
+		remaining = dt.datetime.fromtimestamp(curr_map_end_ms - now).strftime("%M:%S")
+		await message.channel.send(f"Current map is **{curr_map_name}** (**{remaining}** remaining; next map is **{next_map_name}**)")
+	else:
+		await message.channel.send("Unable to retrieve map rotation :)")

28b5e8e2356b60674183786c132dced2822a9786
Andrew Chang <Ajisoo.chang@gmail.com>
2022-09-06 19:32:39 -0700

    fix for real

--- commands/apex_util.py
+++ commands/apex_util.py
@@ -1,46 +1,40 @@
 # import discord
 import datetime as dt
-import time
 
 import requests
 from bs4 import BeautifulSoup
 
 from globals import get_key, API_ENDPOINTS
 
-curr_map_name = None
-curr_map_end_ms = None
-next_map_name = None
-next_map_end_ms = None
+next_time_to_refresh = None
+cached_current_map = None
+cached_next_map = None
 
 
 async def cmd_get_apex_map(message):
 	# should probably be made atomic somehow but who cares
-	global curr_map_name
-	global curr_map_end_ms
-	global next_map_name
-	global next_map_end_ms
-	now = int(time.time())
+	global next_time_to_refresh
+	global cached_current_map
+	global cached_next_map
+	now = dt.datetime.now()
 	# Check cached map data
-	if curr_map_end_ms is not None and now > curr_map_end_ms and next_map_end_ms is not None:
-		# Use cached next map, do request later
-		curr_map_name = next_map_name
-		curr_map_end_ms = next_map_end_ms
-		next_map_name = None
-		next_map_end_ms = None
-	else:
+	if next_time_to_refresh is None or now >= next_time_to_refresh:
 		key = get_key("apex")
 		endpoint_url = API_ENDPOINTS["apex_map"]
 		r = requests.get(endpoint_url, headers={"Authorization": key})
 		if r.status_code == 200:
 			json = r.json()
-			curr_map_name = json["current"]["map"]
+			cached_current_map = json["current"]["map"]
 			curr_map_end_ms = json["current"]["end"]
-			next_map_name = json["next"]["map"]
-			next_map_end_ms = json["next"]["end"]
-			remaining = dt.datetime.fromtimestamp(curr_map_end_ms - now).strftime("%M:%S")
+			cached_next_map = json["next"]["map"]
+			next_time_to_refresh = dt.datetime.fromtimestamp(curr_map_end_ms)
 		else:
 			await message.channel.send("Unable to retrieve map rotation :)")
 			return
 
+	remaining = next_time_to_refresh - now
+	hours, remainder = divmod(remaining.total_seconds(), 3600)
+	minutes, seconds = divmod(remainder, 60)
+	formatted = '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
 	await message.channel.send(
-		f"Current map is **{curr_map_name}** (**{remaining}** remaining; next map is **{next_map_name}**)")
+		f"Current map is **{cached_current_map}** (**{formatted}** remaining; next map is **{cached_next_map}**)")
